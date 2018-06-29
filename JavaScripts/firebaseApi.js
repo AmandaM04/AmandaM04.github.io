52463e1c0396761afda6221e5ea14d4935b72c57
Amanda Mitchell <wreedamitchell@gmail.com>
2018-06-29 13:07:54 -0500

    wip

---
+++ JavaScripts/firebaseApi.js
@@ -0,0 +1,55 @@
+let firebaseConfig = {};
+
+const setConfig = (fbConfig) => {
+  firebaseConfig = fbConfig;
+};
+
+const getAllBlogs = () => {
+  return new Promise((resolve, reject) => {
+    const allBlogsArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/blogs.json`,
+    })
+      .done((allBlogsObject) => {
+        if (allBlogsObject !== null) {
+          Object.keys(allBlogsObject).forEach((fbKey) => {
+            allBlogsObject[fbKey].id = fbKey;
+            allBlogsArray.push(allBlogsObject[fbKey]);
+          });
+        }
+        resolve(allBlogsArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+const getAllProjects = () => {
+  return new Promise((resolve, reject) => {
+    const allProjectsArray = [];
+    $.ajax({
+      method: 'GET',
+      url: `${firebaseConfig.databaseURL}/Projects/projects.json`,
+    })
+      .done((allProjectsObject) => {
+        if (allProjectsObject !== null) {
+          Object.keys(allProjectsObject).forEach((fbKey) => {
+            allProjectsObject[fbKey].id = fbKey;
+            allProjectsArray.push(allProjectsObject[fbKey]);
+          });
+        }
+        resolve(allProjectsArray);
+      })
+      .fail((error) => {
+        reject(error);
+      });
+  });
+};
+
+module.exports = {
+  setConfig,
+  getAllBlogs,
+  getAllProjects,
+};
